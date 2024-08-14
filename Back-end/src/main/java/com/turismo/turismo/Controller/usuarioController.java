eece9206cdb6e9ddc0fcf4801b42f606d3e81f3e
Maidy Conde <maidyladino04@gmail.com>
2024-08-14 13:16:02 -0500

    sdjsdujs

--- Back-end/src/main/java/com/turismo/turismo/Controller/usuarioController.java
+++ Back-end/src/main/java/com/turismo/turismo/Controller/usuarioController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -176,7 +175,7 @@ public class usuarioController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Object> update(@PathVariable String id, @ModelAttribute("Usuario") Usuario UsuarioUpdate) {
+    public ResponseEntity<Object> update(@PathVariable String id, @RequestBody Usuario UsuarioUpdate) {
         var Usuario = usuarioService.findOne(id).get();
         if (Usuario != null) {
             Usuario.setNombreCompleto(UsuarioUpdate.getNombreCompleto());
