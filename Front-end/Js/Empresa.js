523970862eaaa7626cbed8e7433d2e5cbdb91785
Maidy Conde <maidyladino04@gmail.com>
2024-08-22 18:00:17 -0500

    registro empresa

--- Front-end/Js/Empresa.js
+++ Front-end/Js/Empresa.js
@@ -13,7 +13,7 @@ function validarCorreoElectronico(correoElectronico){
 
 var BRegistrarFormularioEmpresa = true;
 
-function resgistroFormularioEmpresa(){
+function registroFormularioEmpresa(){
     var nombreEmpresa = document.getElementById("nombreEmpresa");
     var correoElectronico = document.getElementById("correoElectronico");
     var tipoEmpresa = document.getElementById("tipoEmpresa");
@@ -23,7 +23,7 @@ function resgistroFormularioEmpresa(){
     var nit = document.getElementById("nit");
     var telefono = document.getElementById("telefono");
 
-    if (!ValidarnombreCompletompresa(nombreEmpresa) ||
+    if (!ValidarnombreCompletoempresa(nombreEmpresa) ||
         !ValidarcorreoElectronico(correoElectronico) ||
         !ValidartipoEmpresa(tipoEmpresa) ||
         !ValidarnombreRepresentante(nombreRepresentante) ||
@@ -140,5 +140,328 @@ function verificarcorreoElectronico(correoElectronico, callback){
 }
 
 //Validaciones campos
+
+function ValidarCampos(){
+    var nombreEmpresa = document.getElementById("nombreEmpresa");
+    return ValidarnombreCompletoempresa(nombreEmpresa);
+}
+
+function ValidarnombreCompletoempresa(CuadroNumero){
+    var Valor = CuadroNumero.value;
+    var Valido = true;
+
+    if (Valor.length < 3 || Valor.length > 60) {
+        Valido = false;
+    }
+    if (Valido) {
+        CuadroNumero.className = " input form-control is-valid";
+    } else {
+        CuadroNumero.className = "input form-control is-invalid";
+    }
+    return Valido;
+}
+
+function ValidarCamposcorreoElectronico(){
+    var correoElectronico = document.getElementById("correoElectronico");
+    return ValidarcorreoElectronico(correoElectronico);
+}
+
+function ValidarcorreoElectronico(CuadroNumero){
+    var Valor = CuadroNumero.value;
+    var Valido = true;
+
+    if (Valor.length < 3 || Valor.length > 100) {
+        Valido = false;
+    }
+    if (Valido) {
+        CuadroNumero.className = "input form-control is-valid";
+    } else {
+        CuadroNumero.className = "input form-control is-invalid";
+    }
+    return Valido;
+}
+
+function ValidarCamposTipoEmpresa(){
+    var tipoEmpresa = document.getElementById(" tipoEmpresa");
+    return ValidartipoEmpresa(tipoEmpresa);
+}
+
+function ValidartipoEmpresa(CuadroNumero){
+    var Valor = CuadroNumero.value;
+    var Valido = true;
+
+    if (Valor.length < 3 || Valor.length > 100) {
+        Valido = false;
+    }
+    if (Valido) {
+        CuadroNumero.className = "input form-control is-valid";
+    } else {
+        CuadroNumero.className = "input form-control is-invalid";
+    }
+    return Valido;
+}
+
+function ValidarCamposNombreRepresentante(){
+    var nombreRepresentante = document.getElementById("nombreRepresentante");
+    return ValidarnombreRepresentante(nombreRepresentante);
+}
+
+function ValidarnombreRepresentante(CuadroNumero){
+    var Valor = CuadroNumero.value;
+    var Valido = true;
+
+    if (Valor.length < 3 || Valor.length > 60) {
+        Valido = false;
+    }
+    if (Valido) {
+        CuadroNumero.className = " input form-control is-valid";
+    } else {
+        CuadroNumero.className = "input form-control is-invalid";
+    }
+    return Valido;
+}
+
+function ValidarCamposDireccion(){
+    var direccion = document.getElementById("direccion");
+    return Validardireccion (direccion);
+}
+
+function Validardireccion(CuadroNumero){
+    var Valor = CuadroNumero.value;
+    var Valido = true;
+
+    if (Valor.length < 3 || Valor.length > 100) {
+        Valido = false;
+    }
+    if (Valido) {
+        CuadroNumero.className = "input form-control is-valid";
+    } else {
+        CuadroNumero.className = "input form-control is-invalid";
+    }
+    return Valido;
+}
+
+function ValidarCamposServicios(){
+    var servicios = document.getElementById("servicios");
+    return Validarservicios(servicios);
+}
+
+function Validarservicios(CuadroNumero){
+    var Valor = CuadroNumero.value;
+    var Valido = true;
+
+    if (Valor.length < 3 || Valor.length > 100) {
+        Valido = false;
+    }
+    if (Valido) {
+        CuadroNumero.className = "input form-control is-valid";
+    } else {
+        CuadroNumero.className = "input form-control is-invalid";
+    }
+    return Valido;
+}
+
+function ValidarCamposNit(){
+    var nit = document.getElementById("nit");
+    return Validarnit(nit);
+}
+
+function Validarnit(CuadroNumero){
+    var Valor = CuadroNumero.value;
+    var Valido = true;
+
+    if (Valor.length < 9 || Valor.length > 9) {
+        Valido = false;
+    }
+    if (Valido) {
+        CuadroNumero.className = "input form-control is-valid";
+    } else {
+        CuadroNumero.className = "input form-control is-invalid";
+    }
+    return Valido;
+}
+
+function ValidarCamposTelefono(){
+    var telefono = document.getElementById("telefono");
+    return Validartelefono(telefono);
+}
+
+function Validartelefono(CuadroNumero){
+    var Valor = CuadroNumero.value;
+    var Valido = true;
+
+    if (Valor.length < 7 || Valor.length > 15) {
+        Valido = false;
+    }
+    if (Valido) {
+        CuadroNumero.className = "input form-control is-valid";
+    } else {
+        CuadroNumero.className = "input form-control is-invalid";
+    }
+    return Valido;
+}
+
+//tabla empresa
+
+function listarEmpresa(){
+    $.ajax({
+        url:urlEmpresa,
+        type: "GET",
+        sucess: function (result){
+            var cuerpoTabla = document.getElementById("cuerpoTabla");
+            cuerpoTabla.innerHTML = " ";
+
+            for(var i= 0; i < result.length; i++) {
+                var trRegistro = document.createElement("tr");
+                trRegistro.innerHTML = `
+                <td>${result[i]["idEmpresa"]}</td>
+                <td class="text-center align-middle">${result[i]["nombreEmpresa"]}</td>
+                <td class="text-center align-middle">${result[i]["correoElectronico"]}</td>
+                <td class="text-center align-middle">${result[i]["tipoEmpresa"]}</td>
+                <td class="text-center align-middle">${result[i]["nombreRepresentante"]}</td>
+                <td class="text-center align-middle">${result[i]["direccion"]}</td>
+                <td class="text-center align-middle">${result[i]["servicios"]}</td>
+                <td class="text-center align-middle">${result[i]["nit"]}</td>
+                <td class="text-center align-middle">${result[i]["telefono"]}</td>
+                <td class="text-center align-middle">
+                    <i class="btn fas fa-edit Editar"  onclick="BRegistrarFormularioEmpresa=false;"   data-id="${result[i]["idEmpresa"]}"></i>
+                    <i class="btn fas fa-trash-alt Eliminar" data-id="${result[i]["idEmpresa"]}"></i>
+                </td>
+            `;
+                cuerpoTabla.appendChild(trRegistro);
+            }
+        },
+        error: function (error){
+            alert("ERROR en la peticion" + error);
+
+        }
+    });
+}
+
+var idEmpresa = "";
+
+$(document).on("click", ".Eliminar", function () {
+    var idEmpresa = $(this).data("id"); //PENDIENTE CAMBIARLO SINO FUNCIONA
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: "¿Deseas eliminar este cliente?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Eliminar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: urlEmpresa + "Eliminar/" + idEmpresa,
+                type: "DELETE",
+                success: function (Eliminar) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Registro Eliminado",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    listarEmpresa();
+                },
+                error: function (xhr, status, error) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: 'No se logro eliminar'
+                    });
+                }
+            });
+        }
+    });
+});
+
+
+$(document).on("click", ".Editar", function () {
+    // Limpiar();
+    idEmpresa = $(this).data("id");
+    $.ajax({
+        url: urlEmpresa + idEmpresa,
+        type: "GET",
+        success: function (Empresa) {
+            document.getElementById("nombreEmpresa").value = Empresa.nombreEmpresa;
+            document.getElementById("correoElectronico").value = Empresa.correoElectronico;
+            document.getElementById("tipoEmpresa").value = Empresa.tipoEmpresa;
+            document.getElementById("nombreRepresentante").value = Empresa.nombreRepresentante;
+            document.getElementById("direccion").value = Empresa.direccion;
+            document.getElementById("servicios").value = Empresa.servicios;
+            document.getElementById("nit").value = Empresa.nit;
+            document.getElementById("telefono").value = Empresa.telefono;
+            
+            $('#editUserModal').modal('show');
+        },
+        error: function (error) {
+            alert("Error al obtener el formulario de datos de la empresa: " + error.statusText);
+        }
+    });
+});
+
+function ConsultarEmpresa(id) {
+    $.ajax({
+        url: urlEmpresa + id,
+        type: "GET",
+        success: function (result) {
+            document.getElementById("nombreEmpresa").value = result["nombreEmpresa"];
+            document.getElementById("correoElectronico").value = result["correoElectronico"];
+            document.getElementById("tipoEmpresa").value = result["tipoEmpresa"];
+            document.getElementById("nombreRepresentante").value = result["nombreRepresentante"];
+            document.getElementById("direccion").value = result["direccion"];
+            document.getElementById("servicios").value = result["servicios"];
+            document.getElementById("nit").value = result["nit"];
+            document.getElementById("telefono").value = result["telefono"];
+        }
+    });
+}
+
+function ActualizarEmpresa() {
+    var idEmpresa = document.getElementById("idEmpresa").value;
+    let formData = {
+        "nombreEmpresa": document.getElementById("nombreEmpresa").value,
+        "correoElectronico": document.getElementById("correoElectronico").value,
+        "tipoEmpresa": document.getElementById("tipoEmpresa").value,
+        "nombreRepresentante": document.getElementById("nombreRepresentante").value,
+        "direccion": document.getElementById("direccion").value,
+        "servicios": document.getElementById("servicios").value,
+        "nit": document.getElementById("nit").value,
+        "telefono": document.getElementById("telefono").value,
+
+    };
+
+    if (ValidarCampos) {
+        $.ajax({
+            url: urlEmpresa + idEmpresa,
+            type: "PUT",
+            data: formData,
+            success: function (result) {
+                Swal.fire({
+                    title: "¡Excelente!",
+                    text: "Se guardó correctamente",
+                    icon: "success"
+                });
+                listarEmpresa();
+            },
+            error: function (error) {
+                Swal.fire({
+                    title: "¡Error!",
+                    text: "No se guardó",
+                    icon: "error"
+                });
+            }
+
+        });
+    } else {
+        Swal.fire({
+            title: "¡Error!",
+            text: "Llene todos los campos correctamente",
+            icon: "error"
+        });
+    }
+}
 
 
--- Front-end/Js/config.js
+++ Front-end/Js/config.js
@@ -1,3 +1,3 @@
-var urlBase = "http://10.192.66.33:8080/api/v1/";
+var urlBase = "http://10.192.80.170:8080/api/v1/";
 var urlUsuario = urlBase + "Usuario/";
 var urlEmpresa = urlBase + "Empresa/";
