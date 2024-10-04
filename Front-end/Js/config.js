1a83aa07cca70f2de1734f3efaec418014078adf
Mariana Gonzalez Cal <gonzalezcalderonmariana@gmail.com>
2024-10-04 03:08:05 -0500

    logo

--- Front-end/Js/config.js
+++ Front-end/Js/config.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 var urlBase = "http://10.192.66.45:8082/api/v1/";
-=======
-var urlBase = "http://10.192.92.71:8082/api/v1/";
->>>>>>> d0c8325e75bed3c97c47654cbdda5c3050508f94
 var urlUsuario = urlBase + "Usuario/";
 var urlEmpresa = urlBase + "publico/Empresa/";
 var urlUsuarioPublico= urlBase + "publico/Usuario/";
